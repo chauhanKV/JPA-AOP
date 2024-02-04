a15cd2b32083e324aa45d8a0f779fbade3175857
kavitachauhan <chauhan.kavita@hotmail.com>
2024-02-04 17:10:11 +0530

    annotation

--- src/main/java/com/example/JPAAOP/service/AOPService.java
+++ src/main/java/com/example/JPAAOP/service/AOPService.java
@@ -1,5 +1,6 @@
 package com.example.JPAAOP.service;
 
+import com.example.JPAAOP.aspect.LogExecutionTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ public class AOPService {
 
     public static Logger LOGGER = LoggerFactory.getLogger(AOPService.class);
 
+    @LogExecutionTime
     public String getData()
     {
         LOGGER.info("Returning Data");
